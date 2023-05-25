42eb2fd5069d2a7b65125055709e87d1bf0d4ed9
Iced-Coded <volkogon212@gmail.com>
2023-05-25 10:26:23 +0300

    Added Ice Ingot, Ice Sword and recipes for them.

--- src/main/java/net/mcreator/frostcore/init/FrostcoreModItems.java
+++ src/main/java/net/mcreator/frostcore/init/FrostcoreModItems.java
@@ -11,9 +11,11 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.item.Item;
 
 import net.mcreator.frostcore.item.IceingotItem;
+import net.mcreator.frostcore.item.IceSwordItem;
 import net.mcreator.frostcore.FrostcoreMod;
 
 public class FrostcoreModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, FrostcoreMod.MODID);
+	public static final RegistryObject<Item> ICE_SWORD = REGISTRY.register("ice_sword", () -> new IceSwordItem());
 	public static final RegistryObject<Item> ICEINGOT = REGISTRY.register("iceingot", () -> new IceingotItem());
 }
